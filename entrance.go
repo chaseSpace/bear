abab8fe2104efbecc29cc459fff5cf99801f1d5a
chasespace <no@ex.com>
2025-01-06 13:57:08 +0800

    add NewSet

--- entrance.go
+++ entrance.go
@@ -2,6 +2,7 @@ package bear
 
 import (
 	"github.com/chaseSpace/bear/constraints"
+	"github.com/chaseSpace/bear/sset"
 	"github.com/chaseSpace/bear/sslice"
 )
 
@@ -18,4 +19,9 @@ func NewComputableSlice[T constraints.Computable](data ...T) *sslice.ComputableS
 // NewOrderedSlice creates a new instance of OrderedSlice.
 func NewOrderedSlice[T constraints.Ordered](data ...T) *sslice.OrderedSlice[T] {
 	return sslice.NewOrderedSlice(data...)
+}
+
+// NewSet creates a new instance of Set.
+func NewSet[T comparable](data ...T) *sset.Set[T] {
+	return sset.New(data...)
 }
